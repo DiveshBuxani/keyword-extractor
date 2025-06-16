73e1f0c353538ea19f60581a9fa001947b25cf16
DESKTOP-H87193C\User <crocophobia@gmail.com>
2025-06-16 23:09:50 +0800

    fixed css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 //I use axios here which is a promise-based http client used to make API requests
+import './app.css'
+
 
 
 function App() {
@@ -29,32 +31,35 @@ function App() {
 
   return (
     //main container with basic styling
-    <div style={{ maxWidth: '600px', margin: '2rem auto', fontFamily: 'Arial' }}>
-      <h1>Keyword Extractor</h1>
-      <textarea
-        rows="8"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        placeholder="Enter your text here..."
-        style={{ width: '100%', padding: '0.5rem', marginBottom: '1rem' }}
-      />
-      {/* controlled component tied to text state*/}
-      <button onClick={handleExtract} disabled={loading} style={{ padding: '0.5rem 1rem' }}>
-        {loading ? 'Extracting...' : 'Extract Keywords'}
-      </button>
-      {/* triggers keyword extraction and displays loading state during fetch*/}
+    <div className="outer-div">
+      <div style={{fontFamily: 'Arial', display: 'flex',flexDirection: 'column', maxWidth: '600px', width: '100%', gap: '1rem',}}>
+        <h1 className="header">Keyword Extractor</h1>
+        <textarea
+          rows="8"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="Enter your text here..."
+          style={{ width: '100%', padding: '0.5rem', marginBottom: '1rem' }}
+        />
+        {/* controlled component tied to text state*/}
+        <button onClick={handleExtract} disabled={loading} style={{ }}>
+          {loading ? 'Extracting...' : 'Extract Keywords'}
+        </button>
+        {/* triggers keyword extraction and displays loading state during fetch*/}
 
-      {keywords.length > 0 && (
-        <div style={{ marginTop: '2rem' }}>
-          <h2>Keywords:</h2>
-          <ul>
-            {keywords.map((kw, idx) => (
-              <li key={idx}>{kw}</li>
-            ))}
-          </ul>
-          {/* conditionally renders the list if any are returned */}
-        </div>
-      )}
+
+        {keywords.length > 0 && (
+          <div style={{ marginTop: '2rem' }}>
+            <h2>Keywords:</h2>
+            <ul>
+              {keywords.map((kw, idx) => (
+                <li key={idx}>{kw}</li>
+              ))}
+            </ul>
+            {/* conditionally renders the list if any are returned */}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
